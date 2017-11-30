f0913e75274fcc89be4171eb457ba048498e6b91
chengh <hao.3248@163.com>
2017-11-30 17:49:21 +0800

    updata

--- src/components/voiceWx.vue
+++ src/components/voiceWx.vue
@@ -1,138 +1,156 @@
 <template>
   <div class="container">
-    <button class="btn" type="button" :class="{'working':isWorking}">{{btnText}}</button>
+    <button class="btn" type="button">点击开始吹气</button>
+    <button class="btn" type="button">点击结束吹气</button>
+    <button class="btn" type="button">上传id</button>
     <!--@touchstart.capture="statVoice"  @touchend="stopVoice"-->
   </div>
 </template>
 <script>
-  export default {
-    data () {
-      return {
-        btnText:'点击开始吹气',
-        isWorking:false,
-        voice: {
-          localId: '', //音频的本地ID
-          serverId: '', // 返回音频的服务器端ID
-        },
-        resData: {},
-        appId: 'wxbf6bc8d2ddf651ad',
-      }
-    },
-    mounted(){
-      this.getWxAjax();
-    },
-    methods: {
-      getWxAjax(){
-        const that = this;
-        let host = PUBLICSRC;
-        let option = {
-          url: `/getBaseInfo?url=${PUBLICSRC}`,
-          method: 'get',
-          data: {},
-        };
-        that.axios(option).then((res) => {
+import wx from "weixin-js-sdk";
+
+export default {
+  data() {
+    return {
+      btnText: "点击开始吹气",
+      isWorking: false,
+      voice: {
+        localId: "", //音频的本地ID
+        serverId: "" // 返回音频的服务器端ID
+      },
+      resData: {},
+      appId: "wxbf6bc8d2ddf651ad"
+    };
+  },
+  mounted() {
+    this.getWxAjax();
+  },
+  methods: {
+    getWxAjax() {
+      const that = this;
+      let host = PUBLICSRC;
+      let option = {
+        url: `/getBaseInfo?url=${PUBLICSRC}`,
+        method: "get",
+        data: {}
+      };
+      that
+        .axios(option)
+        .then(res => {
           this.resData = res.data;
           this.wxReady();
-        }).catch((error) => {
+        })
+        .catch(error => {
           alert(error);
         });
-      },
-      wxReady(){
-          const that = this;
-        let configObj = {
-          debug: false,
-          appId: this.appId, // 必填，公众号的唯一标识
-          timestamp: this.resData.timestamp, // 必填，生成签名的时间戳
-          nonceStr: this.resData.noncestr, // 必填，生成签名的随机串
-          signature: this.resData.signature, // 必填，签名
-          jsApiList: ['startRecord', 'stopRecord', 'uploadVoice']
-        };
-        wx.config(configObj);
-        wx.ready(function () {
-          that.$el.children[0].onclick = function () {
-            that.voiceStep();
-          };
-        });
-        wx.error(function (res) {
-          alert(res.errMsg);
-        });
-      },
-      voiceStep(){
-          if(!this.isWorking){
-              this.statVoice();
-          }else {
-              this.stopVoice();
-          }
-      },
-      statVoice(){
-        this.btnText = '再次点击结束吹气';
-        this.isWorking = true;
-        wx.startRecord({
-          cancel: function () {
-            alert('用户拒绝授权录音');
-          }
-        });
-      },
-      stopVoice(){
-        this.btnText = '等待结果';
-        this.isWorking = false;
-        wx.stopRecord({
-          success: function (res) {
-            this.voice.localId = res.localId;
-            alert('语音成功，localId 为' + res.localId);
-            //上传语音
-            wx.uploadVoice({
-              localId: this.voice.localId,
-              success: function (res) {
-                alert('上传语音成功，serverId 为' + res.serverId);
-                this.voice.serverId = res.serverId;
-              }
-            });
-          },
-          fail: function (res) {
-            alert(JSON.stringify(res));
-          }
-        });
-      },
     },
+    wxReady() {
+      const that = this;
+      let configObj = {
+        debug: false,
+        appId: this.appId, // 必填，公众号的唯一标识
+        timestamp: this.resData.timestamp, // 必填，生成签名的时间戳
+        nonceStr: this.resData.noncestr, // 必填，生成签名的随机串
+        signature: this.resData.signature, // 必填，签名
+        jsApiList: ["startRecord", "stopRecord", "uploadVoice"]
+      };
+      wx.config(configObj);
+      wx.ready(function() {
+        alert(JSON.stringify(wx));
+        that.$el.children[0].onclick = function() {
+          that.statVoice();
+        };
+        that.$el.children[1].onclick = function() {
+          that.stopVoice();
+        };
+        that.$el.children[2].onclick = function() {
+          that.upLoad();
+        };
+      });
+      wx.error(function(res) {
+        alert(res.errMsg);
+      });
+    },
+    voiceStep() {
+      if (!this.isWorking) {
+        this.statVoice();
+      } else {
+        this.stopVoice();
+      }
+    },
+    statVoice() {
+      this.btnText = "再次点击结束吹气";
+      this.isWorking = true;
+      wx.startRecord({
+        cancel: function() {
+          alert("用户拒绝授权录音");
+        }
+      });
+    },
+    stopVoice() {
+      this.btnText = "等待结果";
+      this.isWorking = false;
+      alert(JSON.stringify(wx));
+      wx.stopRecord({
+        success: function(res) {
+          this.voice.localId = res.localId;
+          alert("语音成功，localId 为" + res.localId);
+        },
+        fail: function(res) {
+          alert(JSON.stringify(res));
+        }
+      });
+    },
+    upLoad() {
+      //上传语音
+      alert(JSON.stringify(wx));
+      wx.uploadVoice({
+        localId: this.voice.localId,
+        success: function(res) {
+          alert("上传语音成功，serverId 为" + res.serverId);
+          this.voice.serverId = res.serverId;
+        }
+      });
+    }
   }
+};
 </script>
 <style lang="scss" scoped>
-  .container {
-    width: 100%;
-    .btn {
-      outline: none;
-      border: 0;
-      position: relative;
-      display: block;
-      width: 2rem;
-      height: 2rem;
-      margin: 1rem auto;
-      -webkit-box-sizing: border-box;
-      -moz-box-sizing: border-box;
-      box-sizing: border-box;
-      -webkit-border-radius: 100%;
-      -moz-border-radius: 100%;
-      border-radius: 100%;
-      font-size: .2rem;
-      text-align: center;
-      text-decoration: none;
-      color: #FFFFFF;
-      line-height: 2rem;
-      overflow: hidden;
-      background-color: #04be02;
-      /*&:visited {
+.container {
+  width: 100%;
+  .btn {
+    outline: none;
+    border: 0;
+    position: relative;
+    display: block;
+    width: 2rem;
+    height: 2rem;
+    margin: 1rem auto;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    -webkit-border-radius: 100%;
+    -moz-border-radius: 100%;
+    border-radius: 100%;
+    font-size: 0.2rem;
+    text-align: center;
+    text-decoration: none;
+    color: #ffffff;
+    line-height: 2rem;
+    overflow: hidden;
+    background-color: #04be02;
+    /*&:visited {
         color: #FFFFFF;
       }
       &:active {
         animation: myBtn 1s infinite;
         -webkit-animation: myBtn 1s infinite;
       }*/
-      &.working {
-        animation: myBtn 1s infinite;
-        -webkit-animation: myBtn 1s infinite;
-      }
-     /* &:after {
+    &.working {
+      animation: myBtn 1s infinite;
+      -webkit-animation: myBtn 1s infinite;
+    }
+    /* &:after {
         content: " ";
         width: 200%;
         height: 200%;
@@ -153,24 +171,24 @@
         -moz-border-radius: 100%;
         border-radius: 100%;
       }*/
-    }
   }
+}
 
-  @keyframes myBtn {
-    0% {
-      -webkit-box-shadow: 0 0 .2rem 0 #039702;
-      -moz-box-shadow: 0 0 .2rem 0 #039702;
-      box-shadow: 0 0 .2rem 0 #039702;
-    }
-    50% {
-      -webkit-box-shadow: 0 0 .2rem .1rem #039702;
-      -moz-box-shadow: 0 0 .2rem .1rem #039702;
-      box-shadow: 0 0 .2rem .1rem #039702;
-    }
-    100% {
-      -webkit-box-shadow: 0 0 .2rem .2rem #039702;
-      -moz-box-shadow: 0 0 .2rem .2rem #039702;
-      box-shadow: 0 0 .2rem .2rem #039702;
-    }
+@keyframes myBtn {
+  0% {
+    -webkit-box-shadow: 0 0 0.2rem 0 #039702;
+    -moz-box-shadow: 0 0 0.2rem 0 #039702;
+    box-shadow: 0 0 0.2rem 0 #039702;
   }
+  50% {
+    -webkit-box-shadow: 0 0 0.2rem 0.1rem #039702;
+    -moz-box-shadow: 0 0 0.2rem 0.1rem #039702;
+    box-shadow: 0 0 0.2rem 0.1rem #039702;
+  }
+  100% {
+    -webkit-box-shadow: 0 0 0.2rem 0.2rem #039702;
+    -moz-box-shadow: 0 0 0.2rem 0.2rem #039702;
+    box-shadow: 0 0 0.2rem 0.2rem #039702;
+  }
+}
 </style>
--- static/js/config.js
+++ static/js/config.js
@@ -1,6 +1,6 @@
 var PUBLICSRC = window.location.origin + window.location.pathname;
-var HOST = '/xx';
-// var HOST = 'http://wxvoice.ruosi.wang/xx';
+// var HOST = '/xx';
+var HOST = 'http://wxvoice.ruosi.wang/xx';
 var OPENID = '1';
 var tipObj = {
     warn: '信息过短，请重新输入！',
